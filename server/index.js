4ce8086c613116ff71b9887b33f53ce0783f63bb
AlekSShuN <shunikhin92@mail.ru>
2025-09-05 13:19:53 +0300

    feat: add server core files (app.js, data files, update index.js)

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-
 const app = express();
 const PORT = 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Базовые маршруты
 app.get('/api/health', (req, res) => {
     res.json({ message: 'Server is running!', status: 'OK' });
 });
@@ -21,7 +18,7 @@ app.post('/api/appointments', (req, res) => {
     res.json({ message: 'Create new appointment', data: req.body });
 });
 
-// Запуск сервера
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
