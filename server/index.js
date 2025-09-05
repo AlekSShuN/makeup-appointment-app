25a5280892a4f55e9912d4b4c2f1dcf5f89a0db8
AlekSShuN <shunikhin92@mail.ru>
2025-09-05 21:45:23 +0300

    refactoring: changed the error in index.js so that it only starts the server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-const app = express();
-const PORT = 5000;
+import app from './app.js';
 
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/health', (req, res) => {
-    res.json({ message: 'Server is running!', status: 'OK' });
-});
-
-app.get('/api/appointments', (req, res) => {
-    res.json({ message: 'Get all appointments' });
-});
-
-app.post('/api/appointments', (req, res) => {
-    res.json({ message: 'Create new appointment', data: req.body });
-});
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
